1c4130e1b95ebb242d7a640a0b749da417a50fd6
Mojtaba-jahanian <m.jahanian@iau.ir>
2025-11-29 15:25:10 +0330

    Create actor.py

---
+++ src/maddpg/actor.py
@@ -0,0 +1,16 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class Actor(nn.Module):
+    def __init__(self, obs_dim: int, act_dim: int, hidden_dim: int = 64):
+        super().__init__()
+        self.fc1 = nn.Linear(obs_dim, hidden_dim)
+        self.fc2 = nn.Linear(hidden_dim, hidden_dim)
+        self.fc3 = nn.Linear(hidden_dim, act_dim)
+
+    def forward(self, x):
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        return torch.tanh(self.fc3(x))
